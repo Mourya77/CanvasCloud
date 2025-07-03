6502528fc94d3a6d6387ad3dc4ff24769b956e82
Mourya77 <mourya852089@gmail.com>
2025-07-04 01:00:45 +0530

    2nd commit

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -1,12 +1,77 @@
-"use client"
+"use client";
 
+import TaskItem from '@tiptap/extension-task-item'
+import TaskList from '@tiptap/extension-task-list'
+import Table from '@tiptap/extension-table'
+import TableCell from '@tiptap/extension-table-cell'
+import TableHeader from '@tiptap/extension-table-header'
+import TableRow from '@tiptap/extension-table-row'
+import Image from '@tiptap/extension-image'
+import ImageResize from 'tiptap-extension-resize-image'
 import { useEditor, EditorContent } from '@tiptap/react'
 import StarterKit from '@tiptap/starter-kit'
+import { useEditorStore } from '@/store/use-editor-store';
 
 export const Editor = () => {
+    const { setEditor } = useEditorStore();
+
     const editor = useEditor({
-        extensions: [StarterKit],
-        content: '<p>Hello World! 🌏</p>',
+        onCreate({ editor }) {
+            setEditor(editor);
+        },
+        onDestroy() {
+            setEditor(null);
+        },
+        onSelectionUpdate({ editor }) {
+            setEditor(editor);
+        },
+        onTransaction({ editor }) {
+            setEditor(editor);
+        },
+        onFocus({ editor }) {
+            setEditor(editor);
+        },
+        onBlur({ editor }) {
+            setEditor(editor);
+        },
+        onContentError({ editor }) {
+            setEditor(editor);
+        },
+        editorProps: {
+            attributes: {
+                style: "padding-left: 56px; padding-right: 56px;",
+                class: "focus:outline-none print:border-0 bg-white border border-[#C7C7C7] flex flex-col min-h-[1054px] w-[816px] pt-10 pr-14 pb-10 cursor-text"
+            }
+        },
+        extensions: [
+            Table,
+            TableCell,
+            TableHeader,
+            TableRow,
+            StarterKit,
+            Image,
+            ImageResize,
+            TaskList,
+            TaskItem.configure({
+                nested: true,
+            }),
+        ],
+        content: `
+        <table>
+          <tbody>
+            <tr>
+              <th>Name</th>
+              <th colspan="3">Description</th>
+            </tr>
+            <tr>
+              <td>Cyndi Lauper</td>
+              <td>Singer</td>
+              <td>Songwriter</td>
+              <td>Actress</td>
+            </tr>
+          </tbody>
+        </table>
+      `,
     })
 
     return (
